513f6890675008693bcb2f48f80a22cfd7195cd8
student <jsquire4@jhu.edu>
2021-02-19 00:58:49 -0800

    pageable math

--- module4/main.cu
+++ module4/main.cu
@@ -1,4 +1,3 @@
-//Based on the work of Andrew Krepps
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -6,23 +5,60 @@ void doMath(int numBlocks, int totalThreads, int *pos,
             int *rnd, int *added, int *subd, int *multd, 
             int *moded);
 
+__host__ cudaEvent_t get_time(void)
+{
+	cudaEvent_t time;
+	cudaEventCreate(&time);
+	cudaEventRecord(time);
+	return time;
+}
+    
+/**
+ * Allocates pageable memory for host's input and output arrays
+ */
+void pageableMathAlloc(int totalThreads, int **pos,
+                int **rnd, int **added, int **subd, 
+                int **multd, int **moded)
+{
+    int *p, *r, *a, *s, *mu, *mo;
+    p = (int*)malloc(totalThreads*sizeof(int));
+    r = (int*)malloc(totalThreads*sizeof(int));
+    a = (int*)malloc(totalThreads*sizeof(int));
+    s = (int*)malloc(totalThreads*sizeof(int));
+    mu = (int*)malloc(totalThreads*sizeof(int));
+    mo = (int*)malloc(totalThreads*sizeof(int));
+
+    // populate input arrays
+    for (int i=0; i<totalThreads; i++)
+	{
+		p[i] = i;                       
+		r[i] = rand() % 4;
+    } 
+    *pos = p;
+    *rnd = r;
+    *added = a;
+    *subd = s;
+    *multd = mu;
+    *moded = mo;
+}
 
 void main_sub0(int numBlocks, int totalThreads)
 {
     
-    int pos[totalThreads], rnd[totalThreads], added[totalThreads];
-    int subd[totalThreads], multd[totalThreads], moded[totalThreads];
-    
-    // populate input arrays
-	for (int i=0; i<totalThreads; i++)
-	{
-		pos[i] = i;                       
-		rnd[i] = rand() % 4;
-	}
+    int *pos, *rnd, *added, *subd, *multd, *moded;
+    pageableMathAlloc(totalThreads, &pos, &rnd, &added, &subd, &multd, &moded);
+    cudaEvent_t start_time = get_time();
     
     // add, subtract, mult, and mod the two input arrays
     doMath(numBlocks, totalThreads, pos, rnd, added, subd, multd, moded);
 
+    cudaEvent_t end_time = get_time();
+	cudaEventSynchronize(end_time);
+	float delta = 0;
+	cudaEventElapsedTime(&delta, start_time, end_time);
+
+	printf("elapsed time with pageable mem: %3.1f ms\n", delta);
+                                  
     // Save results
     FILE * outFile;
     outFile = fopen("computed_arrays.txt","w");
