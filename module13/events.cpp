e63f19f63b805ea6ce5231bd1300108552bf4d27
jsquires0 <jws422@cornell.edu>
2021-05-03 23:33:56 -0400

    update

--- module13/events.cpp
+++ module13/events.cpp
@@ -18,11 +18,20 @@
 #include <sstream>
 #include <string>
 #include <vector>
-
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h> 
+#ifdef __APPLE__
+#include <OpenCL/cl.h>
+#else
+#include <CL/cl.h>
+#endif
 #include "info.hpp"
+
 
 #define DEFAULT_PLATFORM 0
 #define NUM_BUFFER_ELEMENTS 16
+#define UNIQUE_ARGS 5
 
 // Function to check and handle OpenCL errors
 inline void 
@@ -35,332 +44,306 @@ checkErr(cl_int err, const char * name)
 }
 
 ///
+//  Create an OpenCL context on the first available platform using
+//  either a GPU or CPU depending on what is available.
+//
+cl_context CreateContext()
+{
+    cl_int errNum;
+    cl_uint numPlatforms;
+    cl_platform_id firstPlatformId;
+    cl_context context = NULL;
+
+    // First, select an OpenCL platform to run on.  For this example, we
+    // simply choose the first available platform.  Normally, you would
+    // query for all available platforms and select the most appropriate one.
+    errNum = clGetPlatformIDs(1, &firstPlatformId, &numPlatforms);
+    if (errNum != CL_SUCCESS || numPlatforms <= 0)
+    {
+        std::cerr << "Failed to find any OpenCL platforms." << std::endl;
+        return NULL;
+    }
+
+    // Next, create an OpenCL context on the platform.  Attempt to
+    // create a GPU-based context, and if that fails, try to create
+    // a CPU-based context.
+    cl_context_properties contextProperties[] =
+    {
+        CL_CONTEXT_PLATFORM,
+        (cl_context_properties)firstPlatformId,
+        0
+    };
+    context = clCreateContextFromType(contextProperties, CL_DEVICE_TYPE_GPU,
+                                      NULL, NULL, &errNum);
+    if (errNum != CL_SUCCESS)
+    {
+        std::cout << "Could not create GPU context, trying CPU..." << std::endl;
+        context = clCreateContextFromType(contextProperties, CL_DEVICE_TYPE_CPU,
+                                          NULL, NULL, &errNum);
+        if (errNum != CL_SUCCESS)
+        {
+            std::cerr << "Failed to create an OpenCL GPU or CPU context." << std::endl;
+            return NULL;
+        }
+    }
+
+    return context;
+}
+
+///
+//  Create an OpenCL program from the kernel source file
+//
+cl_program CreateProgram(cl_context context, cl_device_id device, const char* fileName)
+{
+    cl_int errNum;
+    cl_program program;
+
+    std::ifstream kernelFile(fileName, std::ios::in);
+    if (!kernelFile.is_open())
+    {
+        std::cerr << "Failed to open file for reading: " << fileName << std::endl;
+        return NULL;
+    }
+
+    std::ostringstream oss;
+    oss << kernelFile.rdbuf();
+
+    std::string srcStdStr = oss.str();
+    const char *srcStr = srcStdStr.c_str();
+    program = clCreateProgramWithSource(context, 1,
+                                        (const char**)&srcStr,
+                                        NULL, NULL);
+    if (program == NULL)
+    {
+        std::cerr << "Failed to create CL program from source." << std::endl;
+        return NULL;
+    }
+
+    errNum = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
+    if (errNum != CL_SUCCESS)
+    {
+        // Determine the reason for the error
+        char buildLog[16384];
+        clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG,
+                              sizeof(buildLog), buildLog, NULL);
+
+        std::cerr << "Error in kernel: " << std::endl;
+        std::cerr << buildLog;
+        clReleaseProgram(program);
+        return NULL;
+    }
+
+    return program;
+}
+
+///
+//  Create a command queue on the first device available on the
+//  context
+//
+cl_command_queue CreateCommandQueue(cl_context context, cl_device_id *device)
+{
+    cl_int errNum;
+    cl_device_id *devices;
+    cl_command_queue commandQueue = NULL;
+    size_t deviceBufferSize = -1;
+
+    // First get the size of the devices buffer
+    errNum = clGetContextInfo(context, CL_CONTEXT_DEVICES, 0, NULL, &deviceBufferSize);
+    if (errNum != CL_SUCCESS)
+    {
+        std::cerr << "Failed call to clGetContextInfo(...,GL_CONTEXT_DEVICES,...)";
+        return NULL;
+    }
+
+    if (deviceBufferSize <= 0)
+    {
+        std::cerr << "No devices available.";
+        return NULL;
+    }
+
+    // Allocate memory for the devices buffer
+    devices = new cl_device_id[deviceBufferSize / sizeof(cl_device_id)];
+    errNum = clGetContextInfo(context, CL_CONTEXT_DEVICES, deviceBufferSize, devices, NULL);
+    if (errNum != CL_SUCCESS)
+    {
+        delete [] devices;
+        std::cerr << "Failed to get device IDs";
+        return NULL;
+    }
+
+    // In this example, we just choose the first available device.  In a
+    // real program, you would likely use all available devices or choose
+    // the highest performance device based on OpenCL device queries
+    commandQueue = clCreateCommandQueue(context, devices[0], 0, NULL);
+    if (commandQueue == NULL)
+    {
+        delete [] devices;
+        std::cerr << "Failed to create commandQueue for device 0";
+        return NULL;
+    }
+
+    *device = devices[0];
+    delete [] devices;
+    return commandQueue;
+}
+
+///
+//  Create all kernels
+//
+std::vector<cl_kernel> CreateKernels(cl_program program)
+{
+    cl_int errNum;
+    std::vector<cl_kernel> kernels
+
+    for (int i = 0; i < UNIQUE_ARGS; i++)
+    {
+        if (i == 0):
+        {
+            cl_kernel kernel = clCreateKernel(program, "square", &errNum);
+        }
+        else if (i == 1):
+        {
+            cl_kernel kernel = clCreateKernel(program, "cube", &errNum);
+        }
+        else if (i == 1):
+        {
+            cl_kernel kernel = clCreateKernel(program, "identity", &errNum);
+        }
+        else if (i == 1):
+        {
+            cl_kernel kernel = clCreateKernel(program, "double", &errNum);
+        }
+        else:
+        {
+            cl_kernel kernel = clCreateKernel(program, "triple", &errNum);
+        }
+        kernels.push_back(kernel);
+        checkErr(errNum, "clCreateKernel");
+    }
+
+    return kernels;
+}
+
+///
+//  Create memory objects used as the arguments to the kernel
+//
+void CreateBuffers(cl_context context, cl_mem memObjects[5])
+    cl_int errNum;
+{
+    memObjects[0] = clCreateBuffer(context, CL_MEM_READ_WRITE,
+                                    sizeof(int) * NUM_BUFFER_ELEMENTS, 
+                                    NULL, &errNum);
+    memObjects[1] = clCreateBuffer(context, CL_MEM_READ_WRITE,
+                                    sizeof(int) * NUM_BUFFER_ELEMENTS, 
+                                    NULL, &errNum);
+    memObjects[2] = clCreateBuffer(context, CL_MEM_READ_WRITE,
+                                    sizeof(int) * NUM_BUFFER_ELEMENTS, 
+                                    NULL, &errNum);
+    memObjects[3] = clCreateBuffer(context, CL_MEM_READ_WRITE,
+                                    sizeof(int) * NUM_BUFFER_ELEMENTS, 
+                                    NULL, &errNum);
+    memObjects[4] = clCreateBuffer(context, CL_MEM_READ_WRITE,
+                                    sizeof(int) * NUM_BUFFER_ELEMENTS, 
+                                    NULL, &errNum);                                                                                                           
+
+    checkErr(errNum, "clCreateBuffer");                 
+}
+
+///
+// Set up kernels, queue events, and read back result to host 
+// according to command line input
+//
+void setupAndExecute(std::vector<cl_kernel> &kernels, cl_mem buffers[5], 
+                     int arg, cl_context context, cl_command_queue command_queue,
+                     int *inputOutput)
+{
+    cl_int errNum;
+    // Write input data to each buffer
+    for (int i = 0; i < UNIQUE_ARGS; i++)
+    {
+        errNum = clEnqueueWriteBuffer(command_queue, buffers[i], CL_TRUE, 0,
+                                      sizeof(int) * NUM_BUFFER_ELEMENTS, 
+                                      (void*)inputOutput, 0, NULL, NULL);
+    }
+
+    std::vector<cl_event> events;
+    size_t gWI = NUM_BUFFER_ELEMENTS;
+    // Set kernel args and enqueue
+    for (int i = 0; i < UNIQUE_ARGS; i++)
+    {
+        cl_event event;
+        errNum = clSetKernelArg(kernels[i], 0, sizeof(cl_mem), (void *)&buffers[i]);
+        checkErr(errNum, "clSetKernelArg");
+        clEnqueueNDRangeKernel(command_queue, kernels[i], 1, NULL, 
+                               (const size_t*)&gWI, (const size_t*)NULL, 0, 0, 
+                               &event0);
+        events.push_back(event);
+    }
+    // Wait for all events to complete
+ 	clWaitForEvents(UNIQUE_ARGS, &events[0]);
+    // Read back the event according to input arg
+    clEnqueueReadBuffer(command_queue, buffers[arg], CL_TRUE, 0, 
+                        sizeof(int) * NUM_BUFFER_ELEMENTS,
+                        (void*)inputOutput, 0, NULL, NULL);
+
+}
+
+///
 //	main() for OpenCL event assignment. Outputs
 //  different events to host based on input arg
 //
 int main(int argc, char** argv)
 {
     cl_int errNum;
-    cl_uint numPlatforms;
-    cl_uint numDevices;
-    cl_platform_id * platformIDs;
-    cl_device_id * deviceIDs;
-    cl_context context0;
-    cl_program program0;
-    int * inputOutput0;
-    cl_context context1;
-    cl_program program1;
-    int * inputOutput1;
+    cl_context context = 0;
+    cl_program program = 0;
+    cl_command_queue command_queue = 0;
+    cl_device_id device = 0;
+    std::vector<cl_kernel> kernels;
+    cl_mem buffers[5] = {0, 0, 0, 0, 0};
+    int *inputOutput;
 
-    int platform = DEFAULT_PLATFORM; 
+    // Read command line argument
+    int arg;
+	if (argc >= 2) {
+		arg = atoi(argv[1]);
+	}
 
-    // First, select an OpenCL platform to run on.  
-    errNum = clGetPlatformIDs(0, NULL, &numPlatforms);
-    checkErr( 
-        (errNum != CL_SUCCESS) ? errNum : (numPlatforms <= 0 ? -1 : CL_SUCCESS), 
-        "clGetPlatformIDs"); 
- 
-    platformIDs = (cl_platform_id *)alloca(
-            sizeof(cl_platform_id) * numPlatforms);
+    // Create an OpenCL context on first available platform
+    context = CreateContext();
+    // Create OpenCL program from openCL_math.cl kernel source
+    program = CreateProgram(context, device, "events.cl");
+    // Create a command-queue on the first device available
+    // on the created context
+    command_queue = CreateCommandQueue(context, &device);
+    // Create all kernels
+    kernels = CreateKernels(program);
 
-    errNum = clGetPlatformIDs(numPlatforms, platformIDs, NULL);
-    checkErr( 
-       (errNum != CL_SUCCESS) ? errNum : (numPlatforms <= 0 ? -1 : CL_SUCCESS), 
-       "clGetPlatformIDs");
-
-    std::ifstream srcFile("simple.cl");
-    checkErr(srcFile.is_open() ? CL_SUCCESS : -1, "reading simple.cl");
-
-    std::string srcProg(
-        std::istreambuf_iterator<char>(srcFile),
-        (std::istreambuf_iterator<char>()));
-
-    const char * src = srcProg.c_str();
-    size_t length = srcProg.length();
-
-    deviceIDs = NULL;
-    DisplayPlatformInfo(
-        platformIDs[platform], 
-        CL_PLATFORM_VENDOR, 
-        "CL_PLATFORM_VENDOR");
-
-    errNum = clGetDeviceIDs(
-        platformIDs[platform], 
-        CL_DEVICE_TYPE_ALL, 
-        0,
-        NULL,
-        &numDevices);
-    if (errNum != CL_SUCCESS && errNum != CL_DEVICE_NOT_FOUND)
+    // Populate host array
+    inputOutput = new int[NUM_BUFFER_ELEMENTS];
+    for (unsigned int i = 0; i < NUM_BUFFER_ELEMENTS; i++)
     {
-        checkErr(errNum, "clGetDeviceIDs");
-    }       
-
-    deviceIDs = (cl_device_id *)alloca(sizeof(cl_device_id) * numDevices);
-    errNum = clGetDeviceIDs(
-        platformIDs[platform],
-        CL_DEVICE_TYPE_ALL,
-        numDevices, 
-        &deviceIDs[0], 
-        NULL);
-    checkErr(errNum, "clGetDeviceIDs");
-
-    cl_context_properties contextProperties[] =
-    {
-        CL_CONTEXT_PLATFORM,
-        (cl_context_properties)platformIDs[platform],
-        0
-    };
-
-    context0 = clCreateContext(
-        contextProperties, 
-        numDevices,
-        deviceIDs, 
-        NULL,
-        NULL, 
-        &errNum);
-    checkErr(errNum, "clCreateContext");
-
-    context1 = clCreateContext(
-        contextProperties, 
-        numDevices,
-        deviceIDs, 
-        NULL,
-        NULL, 
-        &errNum);
-    checkErr(errNum, "clCreateContext");
- 
-    // Create program from source
-    program0 = clCreateProgramWithSource(
-        context0, 
-        1, 
-        &src, 
-        &length, 
-        &errNum);
-    checkErr(errNum, "clCreateProgramWithSource");
-
-     // Create program from source
-    program1 = clCreateProgramWithSource(
-        context1, 
-        1, 
-        &src, 
-        &length, 
-        &errNum);
-    checkErr(errNum, "clCreateProgramWithSource");
- 
-    // Build program
-    errNum = clBuildProgram(
-        program0,
-        numDevices,
-        deviceIDs,
-        "-I.",
-        NULL,
-        NULL);
- 
-     // Build program
-    errNum |= clBuildProgram(
-        program1,
-        numDevices,
-        deviceIDs,
-        "-I.",
-        NULL,
-        NULL);
- 
-    if (errNum != CL_SUCCESS) 
-    {
-        // Determine the reason for the error
-        char buildLog0[16384];
-        clGetProgramBuildInfo(
-            program0, 
-            deviceIDs[0], 
-            CL_PROGRAM_BUILD_LOG,
-            sizeof(buildLog0), 
-            buildLog0, 
-            NULL);
-
-            std::cerr << "Error in OpenCL C source: " << std::endl;
-            std::cerr << buildLog0;
-            checkErr(errNum, "clBuildProgram");
-     
-        // Determine the reason for the error
-        char buildLog1[16384];
-        clGetProgramBuildInfo(
-            program1, 
-            deviceIDs[0], 
-            CL_PROGRAM_BUILD_LOG,
-            sizeof(buildLog1), 
-            buildLog1, 
-            NULL);
-
-            std::cerr << "Error in OpenCL C source: " << std::endl;
-            std::cerr << buildLog1;
-            checkErr(errNum, "clBuildProgram");
+        inputOutput[i] = i;
     }
 
-    // create buffers and sub-buffers
-    inputOutput0 = new int[NUM_BUFFER_ELEMENTS * numDevices];
-    for (unsigned int i = 0; i < NUM_BUFFER_ELEMENTS * numDevices; i++)
-    {
-        inputOutput0[i] = i;
-    }
- 
-    inputOutput1 = new int[NUM_BUFFER_ELEMENTS * numDevices];
-    for (unsigned int i = 0; i < NUM_BUFFER_ELEMENTS * numDevices; i++)
-    {
-        inputOutput1[i] = i;
-    }
+    // Create buffers for each event
+    CreateBuffers(context, buffers);
 
-    // create a single buffer to cover all the input data
-    cl_mem buffer0 = clCreateBuffer(
-        context0,
-        CL_MEM_READ_WRITE,
-        sizeof(int) * NUM_BUFFER_ELEMENTS * numDevices,
-        NULL,
-        &errNum);
-    checkErr(errNum, "clCreateBuffer");
- 
-     // create a single buffer to cover all the input data
-    cl_mem buffer1 = clCreateBuffer(
-        context1,
-        CL_MEM_READ_WRITE,
-        sizeof(int) * NUM_BUFFER_ELEMENTS * numDevices,
-        NULL,
-        &errNum);
-    checkErr(errNum, "clCreateBuffer");
+    // start timing
+    clock_t start = clock();
+    // Execute kernels and read data back to host as specified by input arg
+    setupAndExecute(kernels, buffers[5], arg, context, command_queue, inputOutput);
+    // end timing
+    clock_t end = clock();
+    double elapsed = double(end - start)/CLOCKS_PER_SEC;
+    std::cout << "Execution time: " << elapsed << "\n" << std::endl;
 
-    // Create command queues
-    InfoDevice<cl_device_type>::display(
-     	deviceIDs[0], 
-     	CL_DEVICE_TYPE, 
-     	"CL_DEVICE_TYPE");
-
-    cl_command_queue queue0 = 
-     	clCreateCommandQueue(
-     	context0,
-     	deviceIDs[0],
-     	0,
-     	&errNum);
-    checkErr(errNum, "clCreateCommandQueue");
- 
-    cl_command_queue queue1 = 
-     	clCreateCommandQueue(
-     	context1,
-     	deviceIDs[0],
-     	0,
-     	&errNum);
-    checkErr(errNum, "clCreateCommandQueue");
- 
-    cl_kernel kernel0 = clCreateKernel(
-     program0,
-     "square",
-     &errNum);
-    checkErr(errNum, "clCreateKernel(square)");
- 
-    cl_kernel kernel1 = clCreateKernel(
-     program1,
-     "cube",
-     &errNum);
-    checkErr(errNum, "clCreateKernel(cube)");
-
-    errNum = clSetKernelArg(kernel0, 0, sizeof(cl_mem), (void *)&buffer0);
-    checkErr(errNum, "clSetKernelArg(square)");
-
-    errNum = clSetKernelArg(kernel1, 0, sizeof(cl_mem), (void *)&buffer1);
-    checkErr(errNum, "clSetKernelArg(cube)");
- 
-    // Write input data
-    errNum = clEnqueueWriteBuffer(
-      queue0,
-      buffer0,
-      CL_TRUE,
-      0,
-      sizeof(int) * NUM_BUFFER_ELEMENTS * numDevices,
-      (void*)inputOutput0,
-      0,
-      NULL,
-      NULL);
- 
-    errNum = clEnqueueWriteBuffer(
-      queue1,
-      buffer1,
-      CL_TRUE,
-      0,
-      sizeof(int) * NUM_BUFFER_ELEMENTS * numDevices,
-      (void*)inputOutput1,
-      0,
-      NULL,
-      NULL);
- 
-    std::vector<cl_event> events;
-    // call kernel for each device
-    cl_event event0;
-
-    size_t gWI = NUM_BUFFER_ELEMENTS;
-
-    errNum = clEnqueueNDRangeKernel(
-      queue0, 
-      kernel0, 
-      1, 
-      NULL,
-      (const size_t*)&gWI, 
-      (const size_t*)NULL, 
-      0, 
-      0, 
-      &event0);
-	
- 	cl_event event1;
- 	errNum = clEnqueueMarker(queue1, &event1);
-
-    errNum = clEnqueueNDRangeKernel(
-      queue1, 
-      kernel1, 
-      1, 
-      NULL,
-      (const size_t*)&gWI, 
-      (const size_t*)NULL, 
-      0, 
-      0, 
-      &event0); 
- 	
- 	//Wait for queue 1 to complete before continuing on queue 0
- 	errNum = clEnqueueBarrier(queue0);
- 	errNum = clEnqueueWaitForEvents(queue0, 1, &event1);
-
- 	// Read back computed data
-   	clEnqueueReadBuffer(
-            queue0,
-            buffer0,
-            CL_TRUE,
-            0,
-            sizeof(int) * NUM_BUFFER_ELEMENTS * numDevices,
-            (void*)inputOutput0,
-            0,
-            NULL,
-            NULL);
-   	clEnqueueReadBuffer(
-            queue1,
-            buffer1,
-            CL_TRUE,
-            0,
-            sizeof(int) * NUM_BUFFER_ELEMENTS * numDevices,
-            (void*)inputOutput1,
-            0,
-            NULL,
-            NULL);
- 
     // Display output in rows
     for (unsigned elems = 0; elems < NUM_BUFFER_ELEMENTS; elems++)
     {
-     std::cout << " " << inputOutput0[elems];
+     std::cout << " " << inputOutput[elems];
     }
     std::cout << std::endl;
- 
-    for (unsigned elems = 0; elems < NUM_BUFFER_ELEMENTS; elems++)
-    {
-     std::cout << " " << inputOutput1[elems];
-    }
-    std::cout << std::endl;
- 
-    std::cout << "Program completed successfully" << std::endl;
 
     return 0;
 }
